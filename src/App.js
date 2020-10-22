3abeb96951faface8b77053a9f22ec2fc9c683e0
Massguy <stefin008@hotmail.co.uk>
2020-10-22 18:23:27 +0100

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-
 import './App.css';
+import Navbar from './Components/Navbar';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+
+
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <h1>Nc-News</h1>
-    </div>
-  );
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+      </Router>
+    </>
+    
+  ); 
 }
 
 export default App;
